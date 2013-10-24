f2a0627bf82248480678fe0192ec4bc20441a405
David Powell <david@drp.id.au>
2013-10-25 00:11:11 +1100

    Put in 0.3 release

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ver=$1
+
+if [ -z "$ver" ]; then
+  echo "Version not supplied!"
+  exit 1
+fi
+
+mkdir -p dist/$ver
+cp -r build/* dist/$ver
+
+cd dist
+for f in $ver/*; do
+  ln -sf $f .
+done
