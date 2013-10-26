a634ee03bd2f206f89c28598bce597bc909115f3
David Powell <david@drp.id.au>
2013-10-26 13:06:57 +1100

    Make build script exit with non-zero edit code on error

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+set -e
+
 rm -rf build
 mkdir build
 
 cp -r css/images build
+cp index.html build
 
-sed -e "s|'\./|'$url|" index.html > build/index.html
-
-browserify -t coffeeify -t hbsfy app/main.coffee | uglifyjs > build/main.js
+echo "Combining css and minifying..."
 cat css/dge.css css/venn.css css/slick.grid.css | cleancss > build/main.min.css
+
+echo "Compiling CoffeeScript and bundling all js..."
+browserify -t coffeeify -t hbsfy app/main.coffee > build/main.big.js 
+echo "Minifying js..."
+uglifyjs build/main.big.js > build/main.js
+rm build/main.big.js
